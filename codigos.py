060047d98b9f6f7fa0b4dba157546efecef2c02d
LucasZarzaBarros <128101194+LucasZarzaBarros@users.noreply.github.com>
2024-06-15 21:47:17 -0300

    Add files via upload

--- codigos.py
+++ codigos.py
@@ -1,7 +1,13 @@
+## Aqui estou importando uma bliblioteca
 import csv
 
+## Aqui estou abrindo um arquivo em txt e abrindo ele como objetivo
 with open ('Personagem e Objetivo.txt') as objetivo:
+    ## Aqui a variavel esta recebendo o valor do objetivo que e meu arquivo txt e esta lendo ele
     historia = objetivo.read()
+
+## Estou printando o Objetivo e a historia do personagem
+print(historia)
 
 ## Aqui e a Primeira funcao que imprime todas as aventuras
 def imprime(copia):
@@ -10,21 +16,27 @@ def imprime(copia):
         ## Aqui e o print que faz a impressao das aventuras
         print(f"Aventura: {i['aventura']}")
 
-##
+## Aqui e a Segunda funcao que imprime apenas uma das aventuras
 def imprimeUm(copia2, j):
+    ## Usei um For para entrar varias vezes no vetor e depois imprimir uma aventura
     for i in copia2:
+        ## usei um if para dizer que quando o i for igual o j (que sifnifica o indice) ele vai printar apenas essa aventura 
         if i == j:
+             ## Aqui e o print de uma aventura
              print(f"Aventura: {i['aventura']}")
 
-
-def apagaUm(copia3, l):
+## Aqui e a Terceira funcao que imprime apenas uma das aventuras
+def apagaUm(copia3, v):
+    ## Usei um For para entrar varias vezes no vetor e depois remover uma das aventuras
     for i in copia3:
-        if i != l:
+        ## Aqui eu estou dizendo que se meu i for diferente do v que significa o indice do vetor ele vai imprimir caso i for igual que v ele não vai entrar ele passara reto
+        if i != v:
+            ## Aqui e o print das aventuras
             print(f"Aventura: {i['aventura']}")
 
 
 
-
+## Tentativa de uma funão que mostra apenas uma aventura e faça o usuario escolher entre Opção A e Opção B
 def extra(copia4, n):
     for i in copia4:
         if i == n:
@@ -36,26 +48,41 @@ def extra(copia4, n):
     escolha = input("Escolha entre A e B: ")
     while escolha != 'a' and escolha != 'A' and escolha != 'b' and escolha != 'B':
         escolha = input("Escolha entre A e B: ") 
-    for i in copia4:
-        if n == "a" and n == "A":
-            print("Você escolheu a opção A")
-            
-        else:
-            print("Você escolheu a Opção B")
 
+    if escolha == "b" and escolha == "B":
+        print("Você escolheu a opção b")
+
+    else:
+        print("Você escolheu a Opção a")
+
+
+## Aqui vai ser a variavel que vai receber a planilha
 plani = []
 
-
+## Aqui estou abrindo a planilha em csv 
 with open ('planilhajogo.csv', 'r') as planilha:
+    ## Aqui eu coloquei a planilha em uma varivel dizendo que ela esta recebendo a planilha 
     aventuras = csv.DictReader(planilha)
 
+    ## Aqui e o for que faz com que as informações da variavel aventuras entre no vetor plani 
     for n in aventuras:
+         ## Aqui estou adicionando um elemento no vetor
          plani.append(n)
         
-
+## Aqui e a variavel que faz com que n tenha o valor atribuido a plani indice [1]
 n = plani[1]
+
+## Aqui e a variavel que faz com que j tenha o valor atribuido a plani indice [3]
 j = plani[3]
+
+## Aqui estou chamando a função extra e passando dois parametros  
 extra(plani, n)
+
+## Aqui estou chamando a Primeira função e estou passando apenas um parametro
 imprime(plani)
+
+## Aqui estou chamando a Segunda função e estou passando dois parametros
 imprimeUm(plani, n)
+
+## Aqui estou chamando a Terceira função e estou passando dois parametros
 apagaUm(plani, j)
